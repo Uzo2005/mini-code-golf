af85cf2751fe6d7e2312441a2e85d6cd1658bf6d
uzo2005 <igwecletus455@gmail.com>
2024-04-01 05:43:37 +0100

    minified stuff

--- main.nim
+++ main.nim
@@ -1,158 +1,78 @@
 import raylib, random, math
-
+template V:untyped=Vector2
+template gm:untyped=getMousePosition()
+template f32:untyped=float32
 var
-    rx = initRand(2005)
-    ry = initRand(2024)
+    rx=initRand(2005)
+    ry=initRand(2024)
+    rfi=loadImage("rf.png")
+    hx=300'i32
+    hy=100'i32
+    bx=60'i32
+    by=400'i32
+    cr=20'f32
+    ipm:bool
+    hb=true
+    bl:f32
+    ba:f32
 const
-    windowWidth = 850
-    windowHeight = 480
-    fieldColor = Lime
-    flagSize = 25
-    wallSize = 50
-    wallColor = getColor(0x0C0A09FF)
-    ballRadius = 10
-    holeRadius = 20
-    originalControlRadius = ballRadius * 2
-    maxControlRadius = originalControlRadius * 3
-
+    W=850
+    H=480
+    B=Black
+    CW=White
 setConfigFlags(flags(Msaa4xHint))
-
-initWindow(windowWidth, windowHeight, "Mini Code Golf")
+initWindow(850,480,"Mini Code Golf")
 setTargetFPS(60)
-
-var redflagImage = loadImage("./red_flag.png")
-
-redflagImage.imageResize(flagSize, flagSize)
-
-let redflag = loadTextureFromImage(redflagImage)
-
-proc drawArrow(x, y, r, n: float32) =
-    drawLine(
-        Vector2(x: x, y: y), Vector2(x: x + (cos(r) * n), y: y - (sin(r) * n)), 3.0, Red
-    )
-
-proc drawBall(x, y: int32, showHighlight: bool) =
-    if showHighlight:
-        drawPolyLines(
-            Vector2(x: float32 x, y: float32 y),
-            10'i32,
-            float32 ballRadius * 4,
-            0'f32,
-            Black,
-        )
-
-    drawCircle(x, y, ballRadius, White)
-
-proc drawHole(x, y: int32) =
-    drawTexture(redflag, x + holeRadius - 2, y - holeRadius, White)
-    drawCircle(x, y, holeRadius, Black)
-
-proc playBall(
-        inPlayMode: var bool,
-        ballX, ballY: var int32,
-        controlRadius, ballShowLine, ballAngle: var float32,
-) =
-    if (int32(getMousePosition().x) in ballX - ballRadius .. ballX + ballRadius) or
-            inPlayMode:
-        inPlayMode = true
+rfi.imageResize(25,25)
+let rf=loadTextureFromImage(rfi)
+proc da(x,y,r,n:f32)=drawLine(V(x:x,y:y),V(x:x+(cos(r)*n),y:y-(sin(r)*n)),3.0,Red)
+proc db(x,y:int32,sh:bool)=
+    if sh:drawPolyLines(V(x:float32 x,y:float32 y),10'i32,40'f32,0'f32,B)
+    drawCircle(x,y,10,CW)
+proc dh(x,y:int32)=
+    drawTexture(rf,x+20-2,y-20,CW)
+    drawCircle(x,y,20,B)
+proc pb(ipm:var bool,bx,by:var int32,cr,bl,ba:var f32)=
+    if (int32(gm.x) in bx-10..bx+10) or ipm:
+        ipm=true
         setMouseCursor(PointingHand)
         if isMouseButtonDown(Left):
-            drawCircleLines(
-                int32 getMousePosition().x,
-                int32 getMousePosition().y,
-                controlRadius,
-                White,
-            )
+            drawCircleLines(int32 gm.x,int32 gm.y,cr,CW)
             if isGestureDetected(Drag):
-                if controlRadius < maxControlRadius:
-                    controlRadius +=
-                        sqrt(
-                            pow(getMousePosition().x - ballX.float32, 2) +
-                                pow(getMousePosition().y - ballY.float32, 2)
-                        ) * 0.008
-
-                if ballShowLine < float32((windowWidth) / 4):
-                    ballShowLine +=
-                        sqrt(
-                            pow(getMousePosition().x - ballX.float32, 2) +
-                                pow(getMousePosition().y - ballY.float32, 2)
-                        ) * 0.008
-
-                ballAngle =
-                    radToDeg(arctan2((getMousePosition().y), (getMousePosition().x))) *
-                    0.2
-
+                if cr<60:
+                    cr+=sqrt(pow(gm.x-bx.float32,2)+pow(gm.y-by.float32,2))*0.008
+                if bl<W/4:
+                    bl+=sqrt(pow(gm.x-bx.float32,2)+pow(gm.y-by.float32,2))*0.008
+                ba=radToDeg(arctan2((gm.y),(gm.x)))*0.2
         else:
-            ballX = ballX + (cos(ballAngle) * ballShowLine).int32
-            ballY = ballY - (sin(ballAngle) * ballShowLine).int32
-            inPlayMode = false
-            controlRadius = originalControlRadius
-            ballShowLine = 0.0
-            ballAngle = 0.0
-    else:
-        setMouseCursor(Arrow)
-
-var
-    holeX: int32 = 300
-    holeY: int32 = 100
-    ballX: int32 = 60
-    ballY: int32 = 400
-    controlRadius: float32 = originalControlRadius
-    inPlayMode: bool
-    highLightBall = true
-    ballShowLine: float32
-    ballAngle: float32
-
-while not (windowShouldClose()):
+            bx=bx+(cos(ba)*bl).int32
+            by=by-(sin(ba)*bl).int32
+            ipm=false
+            cr=20
+            bl=0.0
+            ba=0.0
+    else:setMouseCursor(Arrow)
+while not(windowShouldClose()):
     beginDrawing()
-    clearBackground(fieldColor)
-    drawHole(holeX, holeY)
-    drawBall(ballX, ballY, highLightBall)
-    drawArrow(float32 ballX, float32 ballY, (ballAngle), ballShowLine)
-
-    if (int32(getTime()) mod 2 == 0) and not (inPlayMode):
-        highLightBall = not (highLightBall)
-
-    playBall(inPlayMode, ballX, ballY, controlRadius, ballShowLine, ballAngle)
-
-    if ballX >= windowWidth:
-        ballX -= 10
-    elif ballX <= 0:
-        ballX += 10
-
-    if ballY >= windowHeight:
-        ballY -= 10
-    elif ballY <= 0:
-        ballY += 10
-
-    if checkCollisionCircles(
-        Vector2(x: ballX.float32, y: ballY.float32),
-        ballRadius,
-        Vector2(x: holeX.float32, y: holeY.float32),
-        holeRadius,
-    ):
-        clearBackground(Black)
-        drawText(" WELL DONE! ", windowWidth div 2, windowHeight - 50, 20, RayWhite)
-        drawRectangle((windowWidth div 2), (windowHeight div 2), 200, 60, Red)
-        drawText(
-            "Click To Play Again",
-            (windowWidth div 2) + 30,
-            (windowHeight div 2) + 25,
-            15,
-            RayWhite,
-        )
-
-        if checkCollisionPointRec(
-            getMousePosition(),
-            Rectangle(
-                x: (windowWidth div 2), y: (windowHeight div 2), width: 200, height: 60
-            ),
-        ) and isGestureDetected(Tap):
-            ballX = rx.rand(30 .. windowWidth - 30).int32
-            ballY = ry.rand(30 .. windowHeight - 30).int32
-            holeX = rx.rand(30 .. windowWidth - 30).int32
-            holey = ry.rand(30 .. windowHeight - 30).int32
-
+    clearBackground(Lime)
+    dh(hx,hy)
+    db(bx,by,hb)
+    da(float32 bx,float32 by,ba,bl)
+    if (getTime().int32 mod 2 == 0) and not(ipm):hb=not(hb)
+    pb(ipm,bx,by,cr,bl,ba)
+    if bx>=W:bx-=10
+    elif bx<=0:bx+=10
+    if by>=H:by-=10
+    elif by<=0:by+=10
+    if checkCollisionCircles(V(x:float32 bx, y:float32 by),10,V(x:float32 hx, y:float32 hy),20):
+        clearBackground(B)
+        drawText("WELL DONE!",W div 2,H - 50,20,CW)
+        drawRectangle((W div 2),(H div 2),200,60,Red)
+        drawText("Click To Play Again",(W div 2)+30,(H div 2)+25,15,CW)
+        if checkCollisionPointRec(gm,Rectangle(x:(W div 2),y:(H div 2),width:200,height:60)) and isGestureDetected(Tap):
+            bx=rx.rand(30..W-30).int32
+            by=ry.rand(30..H-30).int32
+            hx=rx.rand(30..W-30).int32
+            hy=ry.rand(30..H-30).int32
     endDrawing()
-
 closeWindow()
---
+++ week11.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "Igwe Cletus"
+description   = "Algorithm Arena Week 11"
+license       = "MIT"
+
+
+# Dependencies
+
+requires "nim == 2.0.2"
+requires "naylib == 4.6.2"
